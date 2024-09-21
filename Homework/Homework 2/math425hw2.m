b1e5958351d97c306054fc5faee80ff52a3a6ab6
mosguinz <mos.guinz@gmail.com>
2024-09-21 02:12:16 -0700

    Add `myRank`

--- Homework/Homework 2/math425hw2.m
+++ Homework/Homework 2/math425hw2.m
@@ -41,8 +41,33 @@ function U = myPartialPivot(A)
         % Eliminate rows.
         for i = j+1:m
             ratio = A(i,j) / A(j,j);
-            A(i,:) = A(i,:) - ratio * A(j,:);
+            format long
+            A(i,:) = A(i,:) - ratio * A(j,:)
         end
     end
     U = A;
 end
+
+% 1(b)
+function r = myRank(A)
+    [m, n] = size(A);
+    if n ~= m
+        error("`A` must be an n x n matrix, got: %d x %d", m, n)
+    end
+    
+    U = myPartialPivot(A);
+    r = 0;
+    for i = 1:n
+        if U(i,i) == 0
+            break
+        end
+        r = r + 1;
+    end
+end
+
+
+% a = [2 -1 2; -1 -1 3; 3 0 -2];
+%a = [1 2 3; 2 4 6; 2 4 6; ];
+ a = [1 2 3; 4 5 6; 7 8 9];
+myRank(a)
+rank(a)
